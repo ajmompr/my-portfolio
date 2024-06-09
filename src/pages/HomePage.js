c3da009621677bb83c30136fe1b0993366ded79f
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 18:06:39 -0500

    moved intro line outside of hero section/grid

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,18 +6,24 @@ import styles from './HomePage.module.css';
 const HomePage = () => {
   return (
     <Container className={styles.hero}>
-       {/* TODO: Update margin */}
-       <br></br>
-      <div className={styles.heroText}>
-        <Typography variant="h2">Hi 👋🏿 My name is Alex Mompremier!</Typography>
-        <Typography variant="body1" className={styles.heroButtons}>
-          {/* TODO: Add aboutme page at later time! */}
-          {/* <Button component={Link} to="/about">About Me</Button> */}
-          <Button component={Link} to="/projects">Projects</Button>
-          <Button component={Link} to="/bookclub">Book Club!</Button>
-        </Typography>
-      </div>
-      <img src="path/to/your/image.jpg" alt="Hero" className={styles.heroImage} />
+      <Typography variant="h3">Hi 👋🏿 My name is Alex Mompremier!</Typography>
+      {/* TODO: Update margin */}
+      <br></br>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12} md={6}>
+          <div className={styles.heroText}>
+            <Typography variant="body1" className={styles.heroButtons}>
+              {/* TODO: Add aboutme page at later time! */}
+              {/* <Button component={Link} to="/about">About Me</Button> */}
+              <Button component={Link} to="/projects">Projects</Button><br></br>
+              <Button component={Link} to="/bookclub">Book Club!</Button>
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img src="path/to/your/image.jpg" alt="Hero" className={styles.heroImage} />
+        </Grid>
+      </Grid>
     </Container>
   );
 };

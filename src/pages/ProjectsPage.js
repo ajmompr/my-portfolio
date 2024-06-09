8a1b8fbd154aa4dc677d5cd21e2190486159e22d
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 17:51:48 -0500

    added to do to fix margin on projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -3,14 +3,16 @@ import { Container, Grid, Card, CardMedia, CardContent, CardActions, Button, Typ
 import styles from './ProjectsPage.module.css';
 
 const projects = [
-  { id: 1, title: 'Project One', description: 'Description of project one', image: 'path/to/image1.jpg' },
-  { id: 2, title: 'Project Two', description: 'Description of project two', image: 'path/to/image2.jpg' },
-  { id: 3, title: 'Project Three', description: 'Description of project three', image: 'path/to/image3.jpg' },
+  { id: 1, title: 'Assignment #1', description: 'An interactive greeting program and random MadLib story generator using core JS principles!', image: '' },
+  { id: 2, title: 'Assignment #3', description: 'An interactive React app that allows users to create a dynamic MadLib story & RGB color blender!', image: '' },
+  { id: 3, title: 'Assignment #6', description: 'An interactive React app that displays CTA route information using parameterized styles and locally-scoped CSS rules!', image: '' },
 ];
 
 const ProjectsPage = () => {
   return (
     <Container>
+      {/* TODO: Update margin */}
+      <br></br>
       <Grid container spacing={4}>
         {projects.map(project => (
           <Grid item key={project.id} xs={12} sm={6} md={4}>

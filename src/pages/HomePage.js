b0f60b06a1cab0728c9fa087e3d5044a32ae60e3
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 18:24:55 -0500

    added introText, projectButton, and bookCLubButton classes for styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,26 +5,26 @@ import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <Container className={styles.hero}>
+    <div className={styles.introText}> 
       <Typography variant="h3">Hi 👋🏿 My name is Alex Mompremier!</Typography>
-      {/* TODO: Update margin */}
-      <br></br>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={12} md={6}>
-          <div className={styles.heroText}>
-            <Typography variant="body1" className={styles.heroButtons}>
+      <Container className={styles.hero}>
+        {/* TODO: Update margin */}
+        <br></br>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <div className={styles.heroText}>
               {/* TODO: Add aboutme page at later time! */}
               {/* <Button component={Link} to="/about">About Me</Button> */}
-              <Button component={Link} to="/projects">Projects</Button><br></br>
-              <Button component={Link} to="/bookclub">Book Club!</Button>
-            </Typography>
-          </div>
+              <Button component={Link} to="/projects" className={styles.projectButton}>Projects</Button><br></br>
+              <Button component={Link} to="/bookclub" className={styles.bookClubButton}>Book Club!</Button>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <img src="path/to/your/image.jpg" alt="Hero" className={styles.heroImage} />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <img src="path/to/your/image.jpg" alt="Hero" className={styles.heroImage} />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </div>
   );
 };
 

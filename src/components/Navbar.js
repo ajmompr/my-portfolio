b9504b65904183cfc5b8dff93769ed70557504f4
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 15:20:15 -0500

    created material UI navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          My Portfolio
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/projects">Projects</Button>
+        <Button color="inherit" component={Link} to="/bookclub">Book Club!</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;

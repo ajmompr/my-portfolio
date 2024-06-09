2cf7e858efcf359f6e0832169c145a1fce326e88
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 15:05:32 -0500

    import navbar component & browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {

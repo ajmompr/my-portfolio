ae1d053960a62511f6603cb7b9b6f541de279d89
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 17:57:23 -0500

    deleted modular navbar stylesheet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={styles.title}>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
           My Portfolio
         </Typography>
-        <Button className={styles.linkButton} component={Link} to="/">Home</Button>
-        <Button className={styles.linkButton} component={Link} to="/projects">Projects</Button>
-        <Button className={styles.linkButton} component={Link} to="/bookclub">Book Club!</Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/projects">Projects</Button>
+        <Button color="inherit" component={Link} to="/bookclub">Book Club!</Button>
       </Toolbar>
     </AppBar>
   );

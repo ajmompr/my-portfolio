a5aea92ac54b4eefc888898507e84f8d94847599
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 18:01:44 -0500

    commented out "about" page route & added TODO

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,9 +9,10 @@ const HomePage = () => {
        {/* TODO: Update margin */}
        <br></br>
       <div className={styles.heroText}>
-        <Typography variant="h2">Welcome to My Portfolio</Typography>
+        <Typography variant="h2">Hi 👋🏿 My name is Alex Mompremier!</Typography>
         <Typography variant="body1" className={styles.heroButtons}>
-          <Button component={Link} to="/about">About Me</Button>
+          {/* TODO: Add aboutme page at later time! */}
+          {/* <Button component={Link} to="/about">About Me</Button> */}
           <Button component={Link} to="/projects">Projects</Button>
           <Button component={Link} to="/bookclub">Book Club!</Button>
         </Typography>

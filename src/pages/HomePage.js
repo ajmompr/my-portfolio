1d672c0d8d9637d2cf8a7b302ff9410b7f2689cb
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 20:22:01 -0500

    added summary report link to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,6 +19,12 @@ const HomePage = () => {
               {/* <Button component={Link} to="/about">About Me</Button> */}
               <Button component={Link} to="/projects" className={styles.projectButton}>Projects</Button><br></br>
               <Button component={Link} to="/bookclub" className={styles.bookClubButton}>Book Club!</Button>
+              <Button
+                href="https://www.canva.com/design/DAGHsT9JIiw/dsKawD_FCmJ3hZ9vGQd6sA/view?utm_content=DAGHsT9JIiw&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.bookClubButton}
+              >Summary Report</Button>
             </div>
           </Grid>
           <Grid item xs={12} md={6}>

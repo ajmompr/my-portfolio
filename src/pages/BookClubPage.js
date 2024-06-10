5edd8a87a8bc4a130dcb5e687859a23e3e89e0b7
Alexandra Mompremier <amompremier@gmail.com>
2024-06-09 21:23:07 -0500

    removed images from book club page

--- src/pages/BookClubPage.js
+++ src/pages/BookClubPage.js
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
+import styles from './BookClubPage.module.css';
 import { Container, Button, Typography, Card, CardContent, CardMedia } from '@mui/material';
-import styles from './BookClubPage.module.css'; // Import the modular stylesheet
 
 // Book recommendations object
 const bookRecommendations = {
   // Create book-topic category & book array key-value
   'Physical Health & Wellness': [
     // TODO add image links & update amazon direct links to purchase!
-    { title: 'Health At Every Size', author: 'Lindo Bacon', image: '', link: 'https://www.amazon.com' },
-    { title: 'Intuitive Eating', author: 'Evelyn Tribole & Elyse Resch', image: '', link: 'https://www.amazon.com' },
-    { title: 'Body Respect', author: 'Lindo Bacon', image: '', link: 'https://www.amazon.com' }
+    { title: 'Health At Every Size', author: 'Lindo Bacon', link: 'https://www.amazon.com' },
+    { title: 'Intuitive Eating', author: 'Evelyn Tribole & Elyse Resch', link: 'https://www.amazon.com' },
+    { title: 'Body Respect', author: 'Lindo Bacon', link: 'https://www.amazon.com' }
   ],
   'Digital Minimalism': [
-    { title: 'Digital Minimalism', author: 'Cal Newport', image: '', link: 'https://www.amazon.com' },
-    { title: 'Deep Work', author: 'Cal Newport', image: '', link: 'https://www.amazon.com' },
-    { title: 'A World Without Email', author: 'Cal Newport', image: '', link: 'https://www.amazon.com' },
-    { title: 'Stolen Focus', author: 'Johann Hari', image: '', link: 'https://www.amazon.com' },
-    { title: 'The Shallows', author: 'Nicholas Carr', image: '', link: 'https://www.amazon.com' },
-    { title: 'Irresistible', author: 'Adam Alter', image: '', link: 'https://www.amazon.com' }
+    { title: 'Digital Minimalism', author: 'Cal Newport', link: 'https://www.amazon.com' },
+    { title: 'Deep Work', author: 'Cal Newport', link: 'https://www.amazon.com' },
+    { title: 'A World Without Email', author: 'Cal Newport', link: 'https://www.amazon.com' },
+    { title: 'Stolen Focus', author: 'Johann Hari', link: 'https://www.amazon.com' },
+    { title: 'The Shallows', author: 'Nicholas Carr', link: 'https://www.amazon.com' },
+    { title: 'Irresistible', author: 'Adam Alter', link: 'https://www.amazon.com' }
   ],
   'Self-Improvement': [
-    { title: 'The Power of Now', author: 'Eckhart Tolle', image: '', link: 'https://www.amazon.com' },
-    { title: 'Quarterlife', author: 'Satya Doyle Bock', image: '', link: 'https://www.amazon.com' },
-    { title: 'How to Be an Imperfectionist', author: 'Stephen Guise', image: '', link: 'https://www.amazon.com' },
-    { title: 'Atomic Habits', author: 'James Clear', image: '', link: 'https://www.amazon.com' }
+    { title: 'The Power of Now', author: 'Eckhart Tolle', link: 'https://www.amazon.com' },
+    { title: 'Quarterlife', author: 'Satya Doyle Bock', link: 'https://www.amazon.com' },
+    { title: 'How to Be an Imperfectionist', author: 'Stephen Guise', link: 'https://www.amazon.com' },
+    { title: 'Atomic Habits', author: 'James Clear', link: 'https://www.amazon.com' }
   ]
 };
 
@@ -32,7 +32,6 @@ const BookClubPage = () => {
   const [book, setBook] = useState(null);
   const [showAllBooks, setShowAllBooks] = useState(false);
 
-  // Function to handle the selection of a topic
   const handleTopicSelection = (topic) => {
     setSelectedTopic(topic);
     const books = bookRecommendations[topic];
@@ -41,14 +40,12 @@ const BookClubPage = () => {
     setBook(randomBook);
   };
 
-  // Function to handle selecting another book
   const handleAnotherBook = () => {
     // Call setSelected to generate another book recommendation for the user
     setSelectedTopic(null);
     setBook(null);
   };
 
-  // Function to handle showing all books
   const handleSeeAllBooks = () => {
     setShowAllBooks(!showAllBooks);
   };
@@ -61,7 +58,7 @@ const BookClubPage = () => {
         <>
           <Typography variant="h4" gutterBottom>Book Club!</Typography>
           <Typography gutterBottom>Please select a category to receive a book recommendation from a list of my favorite reads!</Typography>
-          <Button variant="contained" onClick={handleSeeAllBooks} className={styles.seeAllBooksButton}>
+          <Button variant="contained" onClick={handleSeeAllBooks}>
             See All Books
           </Button><br></br><br></br>
           <Button variant="contained" onClick={() => handleTopicSelection('Self-Improvement')}>
@@ -77,7 +74,7 @@ const BookClubPage = () => {
       ) : showAllBooks ? (
         <>
         <br></br>
-          <Button variant="contained" onClick={handleSeeAllBooks} className={styles.seeAllBooksButton}>
+          <Button variant="contained" onClick={handleSeeAllBooks}>
             Back to Categories
           </Button>
           {Object.keys(bookRecommendations).map((category) => (
@@ -88,9 +85,8 @@ const BookClubPage = () => {
                   <li key={index}>
                     <Card>
                       <CardMedia
-                        component="img"
+    
                         height="140"
-                        image={book.image}
                         alt={book.title}
                       />
                       <CardContent>
@@ -112,9 +108,7 @@ const BookClubPage = () => {
       ) : (
         <Card>
           <CardMedia
-            component="img"
             height="140"
-            image={book.image}
             alt={book.title}
           />
           <CardContent>
@@ -126,7 +120,7 @@ const BookClubPage = () => {
             </Typography>
             <Button variant="contained" href={book.link} target="_blank" className={styles.buyButton}>Buy on Amazon</Button>
             <Button variant="contained" onClick={handleAnotherBook} className={styles.anotherBookButton}>Select Another Book?</Button>
-            <Button variant="contained" onClick={handleSeeAllBooks} className={styles.seeAllBooksButton}>See All Books</Button>
+            <Button variant="contained" onClick={handleSeeAllBooks}>See All Books</Button>
           </CardContent>
         </Card>
       )}
